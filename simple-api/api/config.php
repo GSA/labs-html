6233d6e1d91f06655d9e9a25bde06580ad21687b
alex-perfilov-reisys <alexandr.perfilov@reisystems.com>
2014-01-27 17:41:36 -0500

    adjust script for older PHP versions

--- simple-api/api/config.php
+++ simple-api/api/config.php
@@ -16,5 +16,5 @@ ini_set('display_errors', 1);
 
 //don't change
 define('API_ROOT_PATH', __DIR__.'/');
-define('API_ROOT_URI', parse_url($_SERVER['SCRIPT_URI'], PHP_URL_SCHEME).'://'.$_SERVER["HTTP_HOST"].$base_url);
+define('API_ROOT_URI', parse_url($_SERVER['REQUEST_URI'], PHP_URL_SCHEME).'://'.$_SERVER["HTTP_HOST"].$base_url);
 
